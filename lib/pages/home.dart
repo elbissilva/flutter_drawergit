7cf4a25f8ef71adc570da015b1e509d66a861a32
Elbis Silva <elbissilva@gmail.comm>
2023-06-01 22:50:32 -0300

    commit1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_drawergit/pages/page_cadastro.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -16,40 +17,56 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Colors.green,
           title: const Text("Meu app"),
         ),
-        drawer: const Drawer(
+        drawer: Drawer(
           child: Padding(
-            padding:
-                EdgeInsetsDirectional.symmetric(horizontal: 10, vertical: 5),
+            padding: const EdgeInsetsDirectional.symmetric(
+                horizontal: 10, vertical: 5),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  height: 40,
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    child: const Text("Cadastrar novo cliente"),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const PageCadastro()));
+                  },
                 ),
-                Text("Cadastrar novo cliente"),
-                Divider(
+                const Divider(
                   color: Colors.black,
                 ),
-                SizedBox(height: 20),
-                Text("Clientes cadastrados"),
-                Divider(
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    child: const Text("Cadastrar novo cliente"),
+                  ),
+                  onTap: () {},
+                ),
+                const Divider(
                   color: Colors.black,
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                Text("Histórico de pedidos"),
-                Divider(
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    child: const Text("Cadastrar novo cliente"),
+                  ),
+                  onTap: () {},
+                ),
+                const Divider(
                   color: Colors.black,
                 ),
-                SizedBox(height: 20),
-                Text("Comissões"),
-                Divider(
-                  color: Colors.black,
-                ),
-                SizedBox(height: 20),
-                Text("OP´s Programadas"),
-                Divider(
+                const SizedBox(height: 20),
+                const Divider(
                   color: Colors.black,
                 ),
               ],
