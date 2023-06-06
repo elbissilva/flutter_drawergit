7a2f3ebf336e806f641ff2d517734f478b9493a3
Elbis Silva <elbissilva@gmail.comm>
2023-06-06 20:42:58 -0300

    commit.3

---
+++ lib/pages/page_cadastro.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PageCadastro extends StatelessWidget {
+  const PageCadastro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("TELA DE CADASTRO")),
+    );
+  }
+}
